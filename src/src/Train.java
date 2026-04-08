1c6638b758d91f77462643f5ca37d822192f8973
Samarth230 <samarthalambain@gmail.com>
2026-04-08 08:35:04 +0530

    Use Case 1 for train management STEP

---
+++ src/src/Train.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class UseCase1TrainConsistMgmt {
+
+    public static void main(String[] args) {
+
+        System.out.println("=== Train Consist Management App ===");
+        System.out.println("====================================");
+
+        List<String> trainConsist = new ArrayList<>();
+
+        System.out.println("\nTrain initialized successfully...");
+        System.out.println("Initial Bogie Count : " + trainConsist.size());
+        System.out.println("Current Train Consist : " + trainConsist);
+        System.out.println("\nSystem ready for operations...");
+    }
+}
